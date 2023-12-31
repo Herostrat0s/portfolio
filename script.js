a55f7c8fe3603e2a5f45e4414ca582dd439cd590
Herostrat0s <Muhammed.arslan16@ogr.sakarya.edu.tr>
2023-12-31 15:22:35 +0300

    final

--- script.js
+++ script.js
@@ -5,24 +5,6 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-function copyEmail() {
-  const emailLink = document.querySelector('a[href="mailto:herostratosedu@protonmail.com"]');
-  const emailText = emailLink.textContent;
-
-  // Create a temporary input element
-  const tempInput = document.createElement('input');
-  tempInput.value = emailText;
-  document.body.appendChild(tempInput);
-
-  // Select the text in the input element
-  tempInput.select();
-  tempInput.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text to the clipboard
-  document.execCommand('copy');
-  document.body.removeChild(tempInput);
-
-  // Optionally, you can provide some visual feedback to the user
-  alert('Email copied to clipboard: ' + emailText);
+function openEmail() {
+  window.location.href = "mailto:herostratosedu@protonmail.com";
 }
-
